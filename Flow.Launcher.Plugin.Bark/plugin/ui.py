8cef7ce876f42f0ac283a682ec6f208cd0a983f8
YanCore <zzxnormal@163.com>
2024-07-31 01:27:28 +0800

    修改

--- Flow.Launcher.Plugin.Bark/plugin/ui.py
+++ Flow.Launcher.Plugin.Bark/plugin/ui.py
@@ -6,7 +6,8 @@ from typing import List
 from flowlauncher import FlowLauncher
 
 from plugin.templates import *
-
+import requests
+import webbrowser
 
 class Main(FlowLauncher):
     messages_queue = []
@@ -33,9 +34,15 @@ class Main(FlowLauncher):
         self.messages_queue.append(message)
 
     def query(self, param: str) -> List[dict]:
-        q = param.strip()
-
-        # Do your job at here.
-        ...
-
+        query = q = param.strip()
+        self.sendActionMess(
+            title="Hello World, this is where title goes. {}".format(('Your query is: ' + query , query)[query == '']),
+            subtitle="This is where your subtitle goes, press enter to open Flow's url",
+            method="open_url",
+            values=["https://github.com/Flow-Launcher/Flow.Launcher"],
+        )
+        
         return self.messages_queue
+        
+    def open_url(self, url):
+        webbrowser.open(url)
